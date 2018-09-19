1ed4ae1dcea5f01889165cd83cc4773829fcc451
Nitish Victor <nithishvictor@gmail.com>
2018-09-19 10:50:57 -0400

    Remove GL Init

--- Android2/Android2.NativeActivity/main.cpp
+++ Android2/Android2.NativeActivity/main.cpp
@@ -15,7 +15,7 @@
 *
 */
 
-
+#include <exception>
 #include "AGestureDetector.h"
 #include "Benchmarks.h"
 
@@ -31,7 +31,7 @@ struct saved_state {
 /**
 * Shared state for our app.
 */
-struct engine {
+struct AndroidInstance {
 	struct android_app* app;
 
 	ASensorManager* sensorManager;
@@ -40,131 +40,24 @@ struct engine {
 	AGestureDetector* gestureDetector;
 
 	int animating;
-	EGLDisplay display;
-	EGLSurface surface;
-	EGLContext context;
 	int32_t width;
 	int32_t height;
 	struct saved_state state;
 };
 
-/**
-* Initialize an EGL context for the current display.
-*/
-static int engine_init_display(struct engine* engine) {
-	// initialize OpenGL ES and EGL
-
-	/*
-	* Here specify the attributes of the desired configuration.
-	* Below, we select an EGLConfig with at least 8 bits per color
-	* component compatible with on-screen windows
-	*/
-	const EGLint attribs[] = {
-		EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-		EGL_BLUE_SIZE, 8,
-		EGL_GREEN_SIZE, 8,
-		EGL_RED_SIZE, 8,
-		EGL_NONE
-	};
-	EGLint w, h, format;
-	EGLint numConfigs;
-	EGLConfig config;
-	EGLSurface surface;
-	EGLContext context;
-
-	EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-
-	eglInitialize(display, 0, 0);
-
-	/* Here, the application chooses the configuration it desires. In this
-	* sample, we have a very simplified selection process, where we pick
-	* the first EGLConfig that matches our criteria */
-	eglChooseConfig(display, attribs, &config, 1, &numConfigs);
-
-	/* EGL_NATIVE_VISUAL_ID is an attribute of the EGLConfig that is
-	* guaranteed to be accepted by ANativeWindow_setBuffersGeometry().
-	* As soon as we picked a EGLConfig, we can safely reconfigure the
-	* ANativeWindow buffers to match, using EGL_NATIVE_VISUAL_ID. */
-	eglGetConfigAttrib(display, config, EGL_NATIVE_VISUAL_ID, &format);
-
-	ANativeWindow_setBuffersGeometry(engine->app->window, 0, 0, format);
-
-	surface = eglCreateWindowSurface(display, config, engine->app->window, NULL);
-	context = eglCreateContext(display, config, NULL, NULL);
-
-	if (eglMakeCurrent(display, surface, surface, context) == EGL_FALSE) {
-		LOGW("Unable to eglMakeCurrent");
-		return -1;
-	}
-
-	eglQuerySurface(display, surface, EGL_WIDTH, &w);
-	eglQuerySurface(display, surface, EGL_HEIGHT, &h);
-
-	engine->display = display;
-	engine->context = context;
-	engine->surface = surface;
-	engine->width = w;
-	engine->height = h;
-	engine->state.angle = 0;
-
-	// Initialize GL state.
-	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_FASTEST);
-	glEnable(GL_CULL_FACE);
-	glShadeModel(GL_SMOOTH);
-	glDisable(GL_DEPTH_TEST);
-
-	return 0;
-}
-
-/**
-* Just the current frame in the display.
-*/
-static void engine_draw_frame(struct engine* engine) {
-	if (engine->display == NULL) {
-		// No display.
-		return;
-	}
-
-	// Just fill the screen with a color.
-	/*glClearColor(((float)engine->state.x) / engine->width, engine->state.angle,
-		((float)engine->state.y) / engine->height, 1);*/
-	glClearColor(0, 1, 0, 1);
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	eglSwapBuffers(engine->display, engine->surface);
-}
-
-/**
-* Tear down the EGL context currently associated with the display.
-*/
-static void engine_term_display(struct engine* engine) {
-	if (engine->display != EGL_NO_DISPLAY) {
-		eglMakeCurrent(engine->display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-		if (engine->context != EGL_NO_CONTEXT) {
-			eglDestroyContext(engine->display, engine->context);
-		}
-		if (engine->surface != EGL_NO_SURFACE) {
-			eglDestroySurface(engine->display, engine->surface);
-		}
-		eglTerminate(engine->display);
-	}
-	engine->animating = 0;
-	engine->display = EGL_NO_DISPLAY;
-	engine->context = EGL_NO_CONTEXT;
-	engine->surface = EGL_NO_SURFACE;
-}
 
 /**
 * Process the next input event.
 */
 static int32_t engine_handle_input(struct android_app* app, AInputEvent* event) {
-	struct engine* engine = (struct engine*)app->userData;
+	struct AndroidInstance* engine = (struct AndroidInstance*)app->userData;
 	if (AInputEvent_getType(event) == AINPUT_EVENT_TYPE_MOTION) {
 		engine->state.x = AMotionEvent_getX(event, 0);
 		engine->state.y = AMotionEvent_getY(event, 0);
 		if (engine->gestureDetector->DetectTap(event) == GESTURE_STATE_ACTION) {
 			LOGI("Test Tap");
-			BenchmarkL1ToCPU();
+			//BenchmarkL1ToCPU();
+			//std::terminate();
 		}
 		return 1;
 	}
@@ -175,7 +68,7 @@ static int32_t engine_handle_input(struct android_app* app, AInputEvent* event) 
 * Process the next main command.
 */
 static void engine_handle_cmd(struct android_app* app, int32_t cmd) {
-	struct engine* engine = (struct engine*)app->userData;
+	struct AndroidInstance* engine = (struct AndroidInstance*)app->userData;
 	switch (cmd) {
 	case APP_CMD_SAVE_STATE:
 		// The system has asked us to save our current state.  Do so.
@@ -186,13 +79,13 @@ static void engine_handle_cmd(struct android_app* app, int32_t cmd) {
 	case APP_CMD_INIT_WINDOW:
 		// The window is being shown, get it ready.
 		if (engine->app->window != NULL) {
-			engine_init_display(engine);
-			engine_draw_frame(engine);
+			//engine_init_display(engine);
+			//engine_draw_frame(engine);
 		}
 		break;
 	case APP_CMD_TERM_WINDOW:
 		// The window is being hidden or closed, clean it up.
-		engine_term_display(engine);
+		//engine_term_display(engine);
 		break;
 	case APP_CMD_GAINED_FOCUS:
 		// When our app gains focus, we start monitoring the accelerometer.
@@ -213,7 +106,7 @@ static void engine_handle_cmd(struct android_app* app, int32_t cmd) {
 		}
 		// Also stop animating.
 		engine->animating = 0;
-		engine_draw_frame(engine);
+		//engine_draw_frame(engine);
 		break;
 	}
 }
@@ -224,7 +117,7 @@ static void engine_handle_cmd(struct android_app* app, int32_t cmd) {
 * event loop for receiving input events and doing other things.
 */
 void android_main(struct android_app* state) {
-	struct engine engine;
+	struct AndroidInstance engine;
 
 	memset(&engine, 0, sizeof(engine));
 	state->userData = &engine;
@@ -282,11 +175,6 @@ void android_main(struct android_app* state) {
 
 			}
 
-			// Check if we are exiting.
-			if (state->destroyRequested != 0) {
-				engine_term_display(&engine);
-				return;
-			}
 		}
 
 		if (engine.animating) {
@@ -295,10 +183,6 @@ void android_main(struct android_app* state) {
 			if (engine.state.angle > 1) {
 				engine.state.angle = 0;
 			}
-
-			// Drawing is throttled to the screen update rate, so there
-			// is no need to do timing here.
-			engine_draw_frame(&engine);
 		}
 	}
 }
